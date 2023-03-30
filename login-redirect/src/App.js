272e0472dfb242f6664ab88f4dc06f43f47122e4
Oliver Melvin <obmelvin@gmail.com>
2023-03-30 08:58:42 -0700

    add spinner to demo

--- login-redirect/src/App.js
+++ login-redirect/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { stytchClient } from ".";
+import Spinner from "./Spinner";
 import "./App.css";
 
 const demoForms = [
@@ -15,61 +16,78 @@ const demoForms = [
   },
   {
     title: "Simple Signup",
-    link: "https://form.feathery.io?_slug=NjOv8k",
+    link: "https://templates.feathery.io?_slug=NjOv8k",
     img: "https://feathery.s3.us-west-1.amazonaws.com/templates/Simple+Signup.jpg",
   },
-  // {
-  //   title: "Signup & Onboarding (Robinhood)",
-  //   link: ADD FORM LINK,
-  //   img: "https://feathery.s3.us-west-1.amazonaws.com/templates/Signup+&+Onboarding+(Robinhood).jpg",
-  // },
 ];
 
-function getStytchJwt() {
-  return document.cookie
-    .split("; ")
-    .filter((row) => row.startsWith(`stytch_session_jwt=`))
-    .map((c) => c.split("=")[1])[0];
+function clearStytchDomainCookie() {
+  const domainParts = window.location.hostname.split(".");
+  const domain =
+    domainParts.length === 1
+      ? "localhost"
+      : domainParts.at(-2) + "." + domainParts.at(-1);
+
+  document.cookie = `stytch_session_jwt=; Max-Age=-1; Domain=${domain}`;
+  document.cookie = `stytch_session=; Max-Age=-1; Domain=${domain}`;
 }
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(
-    !!(getStytchJwt() || stytchClient.session.getSync())
+  // If page is refreshed with an existing session, Stytch will have session on
+  // first render so the user can be directly logged in
+  const [loggedIn, setLoggedIn] = useState(!!stytchClient.session.getSync());
+  const [showLoader, setShowLoader] = useState(
+    !loggedIn && document.cookie.includes("stytch_session_jwt")
   );
 
-  stytchClient.session.onChange((newSession) => {
-    setLoggedIn(!!newSession);
+  useEffect(() => {
+    // Fires on new login or logout, but not on refreshing page with existing session
+    const unsubSession = stytchClient.session.onChange((newSession) => {
+      setLoggedIn(!!newSession);
+      setShowLoader(false);
+    });
+    return () => unsubSession && unsubSession();
   });
 
   return (
     <div className="App">
-      <div className="App-header">
-        <h1>Feathery Login Demo</h1>
-        {loggedIn ? (
-          <div className="success">
-            Congratulations, you logged in with a Feathery template!
-            <br />
-            You can log out to try again with another template.
-            <button
-              className="logoutButton"
-              onClick={() => stytchClient.session.revoke()}
-            >
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div className="templates">
-            {demoForms.map(({ title, link, img }) => {
-              return (
-                <div className="template" key={title} onClick={() => window.location.href = link}>
+      {showLoader ? (
+        <Spinner />
+      ) : (
+        <div className="App-header">
+          <h1>Feathery Login Demo</h1>
+          {loggedIn ? (
+            <div className="success">
+              Congratulations, you logged in with a Feathery template!
+              <br />
+              You can log out to try again with another template.
+              <button
+                className="logoutButton"
+                onClick={() => {
+                  stytchClient.session.revoke();
+                  // Clearing the cookie allows for simpler logic regarding the loader
+                  clearStytchDomainCookie();
+                }}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className="templates">
+              {demoForms.map(({ title, link, img }) => (
+                <div
+                  className="template"
+                  key={title}
+                  onClick={() => (window.location.href = link)}
+                >
                   <img className="templateImage" src={img} alt={title} />
                   <span className="templateLabel">{title}</span>
                 </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
