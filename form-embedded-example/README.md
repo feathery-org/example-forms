1a8e465f2e5d4b87e8eddc75cd9da064fafd2f37
bo-dun-1 <pdun2008@gmail.com>
2023-06-10 11:46:04 -0700

    feathery form embedded example

---
+++ form-embedded-example/README.md
@@ -0,0 +1,3 @@
+# Guide
+
+This is an example of a Feathery form embedded into a custom React application.
---
+++ form-embedded-example/src/App.js
@@ -0,0 +1,8 @@
+import { Form } from "@feathery/react";
+import { EMBED_FORM_NAME } from "./FeatheryConfig";
+
+function App() {
+  return <Form formName={EMBED_FORM_NAME} />;
+}
+
+export default App;
