85da295f5a26efe64ad884c2ff24e4f2588df8e3
bo-dun-1 <pdun2008@gmail.com>
2023-06-12 14:11:14 -0700

    update embed form example

--- form-embedded-example/src/App.js
+++ form-embedded-example/src/App.js
@@ -2,7 +2,11 @@ import { Form } from "@feathery/react";
 import { EMBED_FORM_NAME } from "./FeatheryConfig";
 
 function App() {
-  return <Form formName={EMBED_FORM_NAME} />;
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <Form formName={EMBED_FORM_NAME} />
+    </div>
+  );
 }
 
 export default App;
